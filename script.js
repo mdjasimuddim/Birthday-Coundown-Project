a8e013299993710537b9504639ee92fb6b965c30
jasim <jasimuddim231@gmail.com>
2024-01-16 23:27:37 +0600

    Upcoming birthday date is coming

---
+++ script.js
@@ -0,0 +1,23 @@
+const birthday = "1 January 2025";
+
+function countdown(){
+    const birthdayDate = new Date(birthday);
+    const currentDate = new Date();
+
+
+    const totalSeconds = (birthdayDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / (60*60*24));
+    const hours = Math.floor(totalSeconds % (3600*24)/(3600));
+    const minutes = Math.floor(totalSeconds % 3600/60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+    document.getElementById('days').innerText = formatTime(days);
+    document.getElementById('hours').innerText = formatTime(hours);
+    document.getElementById('minutes').innerText = formatTime(minutes);
+    document.getElementById('seconds').innerText = formatTime(seconds);
+}
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time;
+}
+setInterval(countdown, 1000)
+// countdown()
